7313a86d4cc95b622dae0dcf1cadccbdc1c94cb1
Maurits <mcb.westbroek@gmail.com>
2023-09-10 10:04:02 +0100

    Add __repr__s

--- models.py
+++ models.py
@@ -9,9 +9,34 @@ class Project(db.Model):
     date_created = db.db.Column(db.DateTime)
     snippets = db.relationship("Snippet", cascade="all, delete", backref="project")
 
+    def __repr__(self):
+        return f"<Project(id={self.id}, name='{self.name}', date_created='{self.date_created}')>"
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+
 class Snippet(db.Model):
     __tablename__ = "snippets"
 
     id = db.Column(db.Integer, primary_key=True)
     date_created = db.db.Column(db.DateTime)
     project_id = db.db.Column(db.Integer, db.ForeignKey("projects.id"))
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
