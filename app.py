009b27e475126ecfeaca5a8c65c80d6d17372f3d
Maurits <mcb.westbroek@gmail.com>
2023-09-10 14:50:19 +0100

    Add routes for GET: project and POST: snippet

--- app.py
+++ app.py
@@ -17,7 +17,25 @@ def index():
 def get_projects():
     projects = Project.query.all() 
 
-    return render_template("projects.html", projects=projects)
+    formatted_projects = [project.long() for project in projects]
+
+    # return jsonify({
+    #     "projects": formatted_projects
+    # }), 200
+    return render_template("projects.html", projects=formatted_projects)
+
+@requires_permissions("get:projects")
+@app.route("/projects/<int:project_id>")
+def get_project(project_id):
+    project = Project.query.filter(Project.id == project_id).one_or_none()
+
+    if not project:
+        abort(404)
+
+    return jsonify({
+        "success": True,
+        "project": json.dumps(project.long())
+    }), 200
 
 @requires_permissions("post:projects")
 @app.route("/projects", methods=["POST"])
@@ -44,6 +62,31 @@ def post_project():
         "success": True
     }), 200
 
+@requires_permissions("post:snippets")
+@app.route("/projects/<int:project_id>/snippets", methods=["POST"])
+def post_snippet(project_id):
+    # Get form data
+    data = request.json
+
+    try:
+        code, date_created= data["code"], data["date_created"]
+
+    except Exception as e:
+        print(e)
+        abort(400)
+
+    try:
+        new_snippet=Snippet(code=code, date_created=date_created, project_id=project_id)
+        new_snippet.insert()
+
+    except Exception as e:
+        print(e)
+        print(sys.exc_info())
+        abort(500)    
+
+    return jsonify({
+        "success": True
+    }), 200
 
 if __name__ == "__main__":
     app.run()
